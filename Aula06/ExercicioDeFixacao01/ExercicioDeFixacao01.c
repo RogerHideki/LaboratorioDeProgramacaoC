b407fce165312f60eb0998800a4c9087c0b4064f
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-06-09 10:50:15 -0400

    09DeJunho

--- Aula06/ExercicioDeFixacao01/ExercicioDeFixacao01.c
+++ Aula06/ExercicioDeFixacao01/ExercicioDeFixacao01.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <windows.h>
+
+const int tam = 4;
 
 int main() {
-    printf("Teste");
+    SetConsoleOutputCP(65001);
+
+    FILE *fDados;
+    int i;
+    char str[50];
+
+    fDados = fopen(
+            "C:\\Users\\roger\\Documents\\MeusProjetos\\LaboratorioDeProgramacao\\Aula06\\ExercicioDeFixacao01\\dados.txt",
+            "a");
+    if (fDados == NULL) {
+        perror("Erro ao tentar abrir o arquivo\n");
+        return (-1);
+    }
+
+    for (i = 0; i < tam; i++) {
+        printf("Nome da pessoa %d: ", i + 1);
+        scanf(" %[^\n]s", str);
+        fprintf(fDados, "%s | ", str);
+        printf("Idade da pessoa %d: ", i + 1);
+        scanf(" %[^\n]s", str);
+        fprintf(fDados, "%s | ", str);
+        printf("Salário da pessoa %d: ", i + 1);
+        scanf(" %[^\n]s", str);
+        fprintf(fDados, "%s\n", str);
+    }
+
+    fclose(fDados);
 
     return 0;
 }
